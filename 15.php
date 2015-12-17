04e56220a5d32940df667a9e90d774f527e7b2b9
Serg <nextexile@gmail.com>
2015-12-16 21:32:15 -0500

    day 16 was easy so i had time to finish this one

--- 15.php
+++ 15.php
@@ -1,39 +1,40 @@
-<?php        
-        $input = "Sprinkles: capacity 2, durability 0, flavor -2, texture 0, calories 3
-Butterscotch: capacity 0, durability 5, flavor -3, texture 0, calories 3
-Chocolate: capacity 0, durability 0, flavor 5, texture -1, calories 8
-Candy: capacity 0, durability -1, flavor 0, texture 5, calories 8";
+<?php
+    $input = "Sprinkles: capacity 2, durability 0, flavor -2, texture 0, calories 3
+            Butterscotch: capacity 0, durability 5, flavor -3, texture 0, calories 3
+            Chocolate: capacity 0, durability 0, flavor 5, texture -1, calories 8
+            Candy: capacity 0, durability -1, flavor 0, texture 5, calories 8";
 
-        $ingredients = explode("\n", $input);
-        $max_spoons = 100;
-        $highest_score = 0;
+    $max_spoons = 101;
+    $highest_score = 0;
 
-        function formula($first, $second, $third, $fourth)
+    function formula($first, $second, $third, $fourth)
+    {
+        $capacity = max(($first * 2) + ($second * 0) + ($third * 0) + ($fourth * 0), 0);
+        $durability = max(($first * 0) + ($second * 5) + ($third * 0) + ($fourth * -1), 0);
+        $flavor = max(($first * -2) + ($second * -3) + ($third * 5) + ($fourth * 0), 0);
+        $texture = max(($first * 0) + ($second * 0) + ($third * -1) + ($fourth * 5), 0);
+
+        $calories = max(($first * 3) + ($second * 3) + ($third * 8) + ($fourth * 8), 0);
+
+        return $calories == 500 ? $capacity * $durability * $flavor * $texture : 0;
+    }
+
+    for ($f=0; $f < $max_spoons; $f++)
+    {
+        for ($s=0; $s < $max_spoons - $f; $s++)
         {
-            $capacity = max(($first * 2) + ($second * 0) + ($third * -2) + ($fourth * 0), 0);
-            $durability = max(($first * 0) + ($second * 5) + ($third * -3) + ($fourth * 0), 0);
-            $flavor = max(($first * 0) + ($second * 0) + ($third * 5) + ($fourth * -1), 0);
-            $texture = max(($first * 0) + ($second * -1) + ($third * 0) + ($fourth * 5), 0);
-
-            return $capacity * $durability * $flavor * $texture;
-        }
-
-        for ($f=0; $f < $max_spoons; $f++)
-        {
-            for ($s=0; $s < $max_spoons - $f; $s++)
+            for ($t=0; $t < $max_spoons - ($f + $s); $t++)
             {
-                for ($t=0; $t < $max_spoons - ($f + $s); $t++)
+                $o = 100 - ($t + $f + $s);
+                
+                $total = formula($f, $s, $t, $o);
+
+                if($total > $highest_score)
                 {
-                    $o = $max_spoons - ($t + $f + $s);
-
-                    $total = formula($f, $s, $t, $o);
-
-                    if($total > $highest_score)
-                    {
-                        $highest_score = $total;
-                    }
+                    $highest_score = $total;
                 }
             }
         }
+    }
 
-        var_dump($highest_score);
+    var_dump($highest_score);
