03908f2f99473a068386a4266e9721dcc67f4156
Serg <nextexile@gmail.com>
2015-12-25 19:11:34 -0500

    merry christmas

---
+++ 25.php
@@ -0,0 +1,52 @@
+<?php
+
+    $grid = array(
+        array(20151125, 18749137, 17289845, 30943339, 10071777, 33511524),
+        array(31916031, 21629792, 16929656,  7726640, 15514188,  4041754),
+        array(16080970,  8057251,  1601130,  7981243, 11661866, 16474243),
+        array(24592653, 32451966, 21345942,  9380097, 10600672, 31527494),
+           array(77061, 17552253, 28094349,  6899651,  9250759, 31663883),
+        array(33071741,  6796745, 25397450, 24659492,  1534922, 27995004)
+    );
+
+    $row = 0;
+    $col = 0;
+    $target_row = 3010 * 2 - 1;// grid gets filled diagonally, so the value we need
+    $target_col = 3019 * 2 - 1;// isn't present until grid is twice larger, minus one
+
+    function formula($n){ return ($n * 252533) % 33554393; }
+
+    while ($row <= $target_row && $col <= $target_col)
+    {
+        if(!isset($grid[$row][$col]))// skip existing values
+        {
+            if($col == 0)// find prev value for calculation
+            {
+                $prev_col = $row - 1;
+                $prev_row = 0;
+            }
+            else
+            {
+                $prev_row = $row + 1;
+                $prev_col = $col - 1;
+            }
+
+            $grid[$row][$col] = formula($grid[$prev_row][$prev_col]);
+        }
+
+        if($row == 0)// move cursor
+        {
+            $row = $col + 1;
+            if(!isset($grid[$row])){ $grid[$row] = array(); }
+            $col = 0;
+        }
+        else
+        {
+            $row--;
+            $col++;
+        }//var_dump($row, $col, '-------');
+    }
+    var_dump("---------");
+    //var_dump($grid);
+    var_dump($grid[$target_row][$target_col]);
+        
