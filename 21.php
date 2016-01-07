0fe2fd3eec7a35b39124bada23b3128e2e862df6
Serg <nextexile@gmail.com>
2016-01-06 20:33:16 -0500

    trying permutations, will brute force instead

--- 21.php
+++ 21.php
@@ -26,11 +26,35 @@
             "Defense +3" => array(80, 0, 3)
         );
 
+    $everything = array_values(array_merge($weapons, $armor, $rings));
+
     $boss = array("Hit Points" => 104, "Damage" => 8, "Armor" => 1);
-
     $hero = array("Hit Points" => 100, "Damage" => 0, "Armor" => 0);
+    $gold_spent = false;
+    $permutations = gmp_strval(gmp_fact(count($everything)) / gmp_fact((count($everything)) - 3));// 3 types of items
 
-    While($boss['Hit Points'] > 0 & $hero['Hit Points'] > 0)
+    function permute($items, $perms = array( ))
+    {
+        if (empty($items))
+        { 
+            var_dump($perms);
+        }
+        else
+        {
+            for ($i = count($items) - 1; $i >= 0; --$i)
+            {
+                 $newitems = $items;
+                 $newperms = $perms;
+                 list($foo) = array_splice($newitems, $i, 1);
+                 array_unshift($newperms, $foo);
+                 permute($newitems, $newperms);
+             }
+        }
+    }
+
+    permute($everything);
+
+    while($boss['Hit Points'] > 0 & $hero['Hit Points'] > 0)
     {
         $damage_to_hero = $boss["Damage"] - $hero["Armor"];
         $damage_to_boss = $hero["Damage"] - $boss["Armor"];
@@ -38,3 +62,7 @@
         $boss["Hit Points"] -= $damage_to_boss > 0 ? $damage_to_boss : 1;
         $hero["Hit Points"] -= $damage_to_hero > 0 ? $damage_to_hero : 1;
     }
+
+    // var_dump($hero["Hit Points"]);
+    // print "<br/>";
+    // var_dump($boss["Hit Points"]);
