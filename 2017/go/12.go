d7fb9cf8b7ed7d64e251466509ea29a968c60b0c
Serg Chernata <sergchernata@gmail.com>
2017-12-13 16:18:45 -0500

    solves example problem but not the real one

--- 2017/go/12.go
+++ 2017/go/12.go
@@ -6,22 +6,54 @@ import (
 	"strings"
 )
 
+var total = 0
+var programs = make(map[string][]string)
+var seen = make(map[string]bool)
+var start = "0"
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-func main() {
+func traverse(start string) {
 
-	var total = 0
-	var programs = make(map[string][]string)
+	for k,v := range programs {
+
+		if k == start {
+
+			seen[k] = true
+			to_add := len(v)
+
+			for _, p := range v {
+				if seen[p] {
+					to_add--
+				}
+			}
+
+			total += to_add
+
+			for _, p := range v {
+				if !seen[p] {
+					seen[p] = true
+					traverse(p)
+				}
+			}
+
+		}
+
+	}
+
+}
+
+func main() {
 
 	dat, err := ioutil.ReadFile("12_input.txt")
 	check(err)
 
 	// split input
-	data := strings.Split(string(dat), ",")
+	data := strings.Split(string(dat), "\n")
 
 	// parse the data
 	for _, d := range data {
@@ -32,7 +64,10 @@ func main() {
 
 	}
 
+	// 262 too high
+	// 133 too low
+	traverse(start)
 
-	fmt.Println(total, programs)
+	fmt.Println(total)
 
 }
