5e87e5cf23aa1cab43ad4f472c7c5f9a14aa1793
Serg Chernata <sergchernata@gmail.com>
2017-12-20 11:09:30 -0500

    day 15 part B solved

--- 2017/go/15.go
+++ 2017/go/15.go
@@ -10,28 +10,30 @@ func main() {
 
 	var a = 699
 	var b = 124
-	// example
-	// var a = 65
-	// var b = 8921
 	var a_factor = 16807
 	var b_factor = 48271
 	var divisor = 2147483647
 	var matching_pairs = 0
-	var max_pairs = 40000000
+	var max_pairs = 5000000
+	var a_found = []int{}
+	var b_found = []int{}
 
-	for i := 0; i < max_pairs; i++ {
+	for i := 0; len(b_found) < max_pairs; i++ {
 
 		a = a * a_factor % divisor
 		b = b * b_factor % divisor
-		
-		a64 := pad(strconv.FormatInt(int64(a), 2))
-		b64 := pad(strconv.FormatInt(int64(b), 2))
 
-		if a64[16:32] == b64[16:32] {
-			matching_pairs++
+		if a % 4 == 0 {
+			a_found = append(a_found, a)
+		}
+
+		if b % 8 == 0 {
+			b_found = append(b_found, b)
 		}
 
 	}
+
+	matching_pairs = judge(a_found, b_found)
 
 	fmt.Println(matching_pairs)
 
@@ -43,4 +45,24 @@ func pad(binary string) string {
 		binary = "0"+binary
 	}
 	return binary
+}
+
+func judge(a_found []int, b_found []int) int {
+
+	matches := 0
+	count := len(b_found)
+
+	for i := 0; i < count; i++ {
+
+		a_padded := pad(strconv.FormatInt(int64(a_found[i]), 2))
+		b_padded := pad(strconv.FormatInt(int64(b_found[i]), 2))
+
+		if a_padded[16:] == b_padded[16:] {
+			matches++
+		}
+
+	}
+
+	return matches
+
 }
