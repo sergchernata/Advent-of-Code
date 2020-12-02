ab1ee8abbd9602b89d1d642fafec1ae7671c8c6a
Serg Chernata <sergchernata@gmail.com>
2020-12-02 08:52:46 -0500

    added day 2 solutions

---
+++ 2020/elixir/2.ex
@@ -0,0 +1,34 @@
+defmodule Day1 do
+	def part1(input) do
+		input
+		|> Enum.filter(fn [_, min, max, letter, password] ->
+				String.match?(password, ~r/^([^#{letter}]*#{letter}[^#{letter}]*){#{min},#{max}}$/)
+			end)
+		|> Enum.count
+	end
+
+	def part2(input) do
+		input
+		|> Enum.filter(fn [_, min, max, letter, password] ->
+				chars = String.codepoints(password)
+				first = Enum.at(chars, String.to_integer(min) - 1)
+				second = Enum.at(chars, String.to_integer(max) - 1)
+				(first == letter && second != letter) || (first != letter && second == letter)
+			end)
+		|> Enum.count
+	end
+
+	def load(file) do
+		case File.read(file) do
+		  {:ok, body} -> body 
+		  				 |> String.split("\n", trim: true)
+		  				 |> Enum.map(& Regex.run(~r/(\d+)-(\d+) ([a-z]): ([a-z]*)/, &1))
+		  {:error, _} -> IO.puts "Couldn't open the file."
+		end
+	end
+end
+
+input = Day1.load("2.input.txt")
+
+input |> Day1.part1 |> IO.inspect # 515
+input |> Day1.part2 |> IO.inspect # 711
